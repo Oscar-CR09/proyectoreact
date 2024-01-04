89156c7954c0707e61351ada069ed316595c65a9
Oscar <oscarcr189@outlook.com>
2024-01-03 21:04:22 -0600

    componentes

---
+++ vite-project/src/components/HelloWorld.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+export function HelloWorld (){
+
+    return( 
+
+        <Fragment> 
+
+            <div>Hola Mundo</div>
+            <div>Que tal</div>
+
+        </Fragment>
+
+     );
+   
+}
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { HelloWorld } from './components/HelloWorld'
 import './index.css'
 
-//const h1 = React.createClass('div',null, React.createElement('ul',null, React.createElement('li',null,'item 1' )
-const h1 = <div><ul><li>Hola mundo</li></ul> </div>
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 
 
- // <React.StrictMode>
+  <React.StrictMode>
+    <HelloWorld />
+  </React.StrictMode>,
 
-  //  <div>Hola Mundo</div>
- //   <App />
-// </React.StrictMode>,
-h1
 
 )
