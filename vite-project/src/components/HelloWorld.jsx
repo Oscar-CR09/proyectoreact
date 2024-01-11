42759e61168bab164a372e1a3acacbb8806625f0
Oscar <oscarcr189@outlook.com>
2024-01-10 20:54:02 -0600

    componentes por defectos

--- vite-project/src/components/HelloWorld.jsx
+++ vite-project/src/components/HelloWorld.jsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import  PropTypes  from 'prop-types';
 
-export const HelloWorld = ( {user, id, title } ) => {
+export const HelloWorld = ( {user, id, title, book } ) => {
 
     console.log(title)
     //const name ='pepe';
@@ -12,6 +12,7 @@ export const HelloWorld = ( {user, id, title } ) => {
 
             <div> { title } </div>
             <div> Que tal! { user.name }{user.lastName} con el id { id } </div>
+            <div>{book}</div>
 
 
         </Fragment>
@@ -24,4 +25,10 @@ HelloWorld.PropTypes = {
     title:PropTypes.string.isRequired,
     id:PropTypes.number.isRequired,
     user:PropTypes.object
+}
+
+HelloWorld.defaultProps = {
+    title: 'Hola mundo por defecto!',
+    book: 'uml go a gota'
+
 }
